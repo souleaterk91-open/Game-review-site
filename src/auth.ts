b9ebff6295d605af18c5ea0cf87554755c9c7e4d
Yoojin Kim <souleaterk91@gmail.com>
2026-02-23 05:12:55 +0900

    update

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import prisma from "@/lib/db"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
